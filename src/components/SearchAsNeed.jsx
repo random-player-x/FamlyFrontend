a2e563c6f56bdc2be28df6b2212aa944f85b2a2a
Keshav-Raj-098 <keshavraj09898@gmail.com>
2025-01-25 16:28:47 +0530

    footer added

--- src/components/SearchAsNeed.jsx
+++ src/components/SearchAsNeed.jsx
@@ -1,55 +1,52 @@
-import mobile1 from '../images/mobile1.png';
-import mobile2 from '../images/mobile2.png';
-import mobile3 from '../images/mobile3.png';
+import mobile1 from '../images/Home/a.png';
+import mobile2 from '../images/Home/b.png';
+import mobile3 from '../images/Home/c.png';
 import mobile4 from '../images/mobile4.png';
 
 
 const SearchAsNeed = () => {
     return (
         <div className='mt-[10%]'>
-        <div className="h-auto flex bg-white/80 justify-center">
-            <div className="flex h-[80vh] rounded-full bg-orange-50 justify-center items-center w-[50%] font-bold">
-                <div className=' p-5 h-full flex flex-col justify-center'>
-                    <img src={mobile1} className='' alt="" />
+            <div className="h-auto flex bg-white/80 justify-center">
+                <div className="flex h-[80vh] rounded-full bg-orange-50 justify-center items-center w-[50%] font-bold">
+                    <img src={mobile1} alt="img" className='md:h-[500px] h-[250px]' />
+
                 </div>
-                <div className=' p-5 h-full flex flex-col justify-end pb-10'>
-                <img src={mobile2} alt="" />
+                <div className="w-[40%] flex flex-col justify-center gap-4 p-10">
+                    <span className="text-4xl font-bold">
+                        Search as per your Needs
+                    </span>
+                    <span className="text-xl">
+                        Customised your search for your special family based on your needs
+                    </span>
                 </div>
             </div>
-            <div className="w-[40%] flex flex-col justify-center gap-4 p-10">
-            <span className="text-4xl font-bold">
-                Search as per your Needs 
-                </span>
-                <span className="text-xl">
-                Customised your search for your special family based on your needs
-                </span>
-            </div>
-        </div>
-        <KnowMoreFamily/>
-        <AiDriven/>
+            <KnowMoreFamily />
+            <AiDriven />
         </div>
     );
-    
-    
+
+
 }
 
-const KnowMoreFamily = ()=> {
+const KnowMoreFamily = () => {
     return (
         <div className="h-auto flex bg-white/80 justify-center">
-             <div className="w-[40%] flex flex-col justify-center gap-4 p-10">
-             <span className="text-4xl font-bold">
-                Know more about the Family
+            <div className="w-[40%] flex flex-col justify-center gap-4 p-10">
+                <span className="text-4xl font-bold">
+                    Know more about the Family
                 </span>
                 <span className="text-xl">
-                Get informed about the the members of the family you will be spending your life with
+                    Get informed about the the members of the family you will be spending your life with
                 </span>
             </div>
             <div className="flex h-[80vh] bg-orange-50 rounded-full justify-center items-center w-[50%] font-bold">
-                <div className=' p-5 h-full flex flex-col justify-center'>
-                    <img src={mobile3} className='' alt="" />
+                <div className="flex h-[80vh] rounded-full bg-orange-50 justify-center items-center w-[50%] font-bold">
+                    <img src={mobile2} alt="img" className='md:h-[500px] h-[250px]' />
+
                 </div>
                 <div className=' p-5 h-full flex flex-col justify-end pb-10'>
-                <img src={mobile4} alt="" />
+                    <img src={mobile4} alt="" />
                 </div>
             </div>
 
@@ -58,24 +55,20 @@ const KnowMoreFamily = ()=> {
 
 }
 
-const AiDriven = ()=> {
+const AiDriven = () => {
     return (
         <div className="h-auto flex bg-white/80 justify-center">
 
             <div className="flex h-[80vh] rounded-full bg-orange-50 justify-center items-center w-[50%] font-bold">
-                <div className=' p-5 h-full flex flex-col justify-center'>
-                    <img src={mobile3} className='' alt="" />
-                </div>
-                <div className=' p-5 h-full flex flex-col justify-end pb-10'>
-                <img src={mobile4} alt="" />
-                </div>
+                <img src={mobile3} alt="img" className='md:h-[500px] h-[250px]' />
+
             </div>
             <div className="w-[40%] flex flex-col justify-center gap-4 p-10">
-             <span className="text-4xl font-bold">
-                 AI-driven match recommendations to suit your preferences
+                <span className="text-4xl font-bold">
+                    AI-driven match recommendations to suit your preferences
                 </span>
                 <span className="text-xl">
-                Get the perfect Match with the help of our Ai- recommendation engine which gives profile suggestions based on your preferneces
+                    Get the perfect Match with the help of our Ai- recommendation engine which gives profile suggestions based on your preferneces
                 </span>
             </div>
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { MainBox } from '../components/Headings';
 import { WeDifferent } from '../components/WhychooseUs';
 // import { Testimonials } from '../components/Testimonials';
 import SearchAsNeed from '../components/SearchAsNeed';
-import { Footer } from '../components/Footer';
+import Footer  from '../components/Home/Footer';
 import  GooglePlay  from '../components/GooglePlay';
 import BottomDescription from '../components/BottomDescription';
 function HomePage() {
