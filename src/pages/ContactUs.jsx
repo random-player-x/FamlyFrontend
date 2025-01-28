5b5df073330e67a51dddc65bc6bbb090aa6a9146
Utkarsh <utkarsh@Utkarshs-MacBook-Air-1381.local>
2025-01-29 00:10:30 +0530

    added

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -206,6 +206,7 @@ const ContactForm = () => {
             </div>
             <div>
               <button
+                // onClick={handleSubmit}
                 type="submit"
                 className="w-full bg-orange-500 text-white py-3 px-6 rounded-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 transition-colors"
               >
