759f10e1dcc492d45c658783a428824f420ea031
Utkarsh <utkarsh@Utkarshs-MacBook-Air-1381.local>
2025-02-05 18:31:25 +0530

    complete

--- src/pages/UserinfoPage.jsx
+++ src/pages/UserinfoPage.jsx
@@ -174,7 +174,7 @@ const UserProfile = () => {
             onClick={() => setShowDisableModal(true)}
             className="px-6 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors"
           >
-            Disable Account
+            Delete Account
           </button>
         </div>
 
