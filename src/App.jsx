dd504e3e62f4f0ad70f35acf85f09b245426710b
Utkarsh <utkarsh@Utkarshs-MacBook-Air-1381.local>
2025-02-08 22:01:40 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import HomePage from './pages/HomePage';
 import PrivacyPolicy from './pages/PrivacyPolicy';
